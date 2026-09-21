8ccac5e346fc8a3ec1a394b3cf0d9fec91885830
haseeb-mughal18 <haseebmughalmsb@gmail.com>
2026-09-21 19:55:10 +0500

    added whatsapp Link

--- src/data/socials.js
+++ src/data/socials.js
@@ -1,9 +1,12 @@
+  // import Contact from "../personal/data";
+
+  const Contact = "+923052114394";
 export const socials = [
-  {
-    name: "Twitter",
-    url: "https://twitter.com/",
-    path: "M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z",
-  },
+  // {
+  //   name: "Twitter",
+  //   url: "https://twitter.com/",
+  //   path: "M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z",
+  // },
   {
     name: "LinkedIn",
     url: "https://www.linkedin.com/in/codewithhaseeb",
@@ -16,7 +19,7 @@ export const socials = [
   },
   {
     name: "whatsapp",
-    url: "https://whatsapp.com/",
+    url: `https://wa.me/${Contact}?text=Hello%20I%20would%20like%20to%20chat`,
     render: "whatsapp",
   },
 ];
---
+++ src/personal/data.js
@@ -0,0 +1,1 @@
+export const Contact = "+92 305 2114394";
